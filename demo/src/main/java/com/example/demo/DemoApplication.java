1a3884d0ae738884c81521e4f04bc293f05a9830
Fadi Youssef Akkaoui <fadi.youssef.akkaoui@wirelesscar.com>
2023-01-19 15:41:15 +0100

    CRUD added, connection to postgres and one scheduler

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
+	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 2000L)
+	public void exampleScheduler(){
+		logger.info("Job Current Time " + new Date());
 	}
 
 }
